91c4849cd8191ea3652d2ac40eca895b40d06193
sz-m <sz.mihu@gmail.com>
2023-12-29 21:10:13 +0100

    5 napos előrejelzés módosítva

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ import streamlit as st
 import time
 import pandas
 
-
-
-
 API_KEY = st.text_input("API kulcs openweathermap.org oldalhoz", type="password")
 
 if API_KEY:
@@ -19,26 +16,20 @@ if API_KEY:
     nr_of_data = 8 * days
     dayly_data = filtered_data[:nr_of_data]
     firstdata = dayly_data[0]
-    firstday = dayly_data[:8]
-    temperature = [dict["main"]["temp"] for dict in firstday]
-    hours = [h["dt_txt"][11:16] for h in firstday]
-    icons = [i["weather"][0]["icon"] for i in firstday]
-    descriptions = [i["weather"][0]["description"] for i in firstday]
+    day1 = dayly_data[:8]
+    day2 = dayly_data[8:16]
+    day3 = dayly_data[16:24]
+    day4 = dayly_data[24:32]
+    day5 = dayly_data[32:40]
+    temperature = [dict["main"]["temp"] for dict in day1]
+    hours = [h["dt_txt"][11:16] for h in day1]
+    icons = [i["weather"][0]["icon"] for i in day1]
+    descriptions = [i["weather"][0]["description"] for i in day1]
     now = time.strftime("%H:%M")
-    maxok = []
-    minek = []
     napok = []
-    desc1 = []
-    desc2 = []
+
     for i in dayly_data:
-        if i["dt_txt"][11:13] == "12":
-            maxok.append(i["main"]["temp_max"])
-            napok.append(i["dt_txt"][5:10])
-            desc1.append(i["weather"][0]["description"])
-    for i in dayly_data:
-        if i["dt_txt"][11:13] == "00":
-            minek.append(i["main"]["temp_min"])
-            desc2.append(i["weather"][0]["description"])
+        napok.append(i["dt_txt"][5:10])
 
     with st.container():
         col1, col2 = st.columns(2)
@@ -71,17 +62,30 @@ if API_KEY:
         if gomb:
             st.balloons()
 
-    tab1, tab2, tab3, tab4, tab5 = st.tabs([f"{napok[0]}", f"{napok[1]}", f"{napok[2]}",
-                                            f"{napok[3]}", f"{napok[4]}"])
+    tab1, tab2, tab3, tab4, tab5 = st.tabs([f"{napok[0]}", f"{napok[7]}", f"{napok[15]}",
+                                            f"{napok[23]}", f"{napok[31]}"])
     tabs = [tab1, tab2, tab3, tab4, tab5]
+    days = [day1, day2, day3, day4, day5]
     szam = 0
-    for tetel in tabs:
-        with tetel:
-            st.write(f"nappal {int(maxok[szam])}°C, {desc1[szam]}")
-            st.write(f"éjjel {int(minek[szam])}°C, {desc2[szam]}")
-        szam += 1
 
-    chart_data = pandas.DataFrame({"hőfok":[dict["main"]["temp"] for dict in dayly_data],
-                "dátum":[dict["dt_txt"] for dict in dayly_data]})
+    for item in tabs:
+        with item:
+            temperature = [dict["main"]["temp"] for dict in days[szam]]
+            hours = [h["dt_txt"][11:16] for h in days[szam]]
+            icons = [i["weather"][0]["icon"] for i in days[szam]]
+            descriptions = [i["weather"][0]["description"] for i in days[szam]]
+            c1, c2, c3, c4, c5, c6, c7, c8 = st.columns(8)
+            cols = [c1, c2, c3, c4, c5, c6, c7, c8]
+            index = 0
+            for i in cols:
+                with i:
+                    i.write(hours[index])
+                    i.write(f"{int(temperature[index])} °C")
+                    i.write(descriptions[index])
+                    index = index + 1
+            szam = szam + 1
+
+    chart_data = pandas.DataFrame({"hőfok": [dict["main"]["temp"] for dict in dayly_data],
+                                   "dátum": [dict["dt_txt"] for dict in dayly_data]})
     st.subheader("Hőmérsékletváltozás")
     st.bar_chart(chart_data, x="dátum", y="hőfok", color=[0.0, 1.0, 0.0])
